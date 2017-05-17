0ab5c7ccce70d259d1e8e7fdc83c470c383d749a
waqarhassans <waqar.hassan@devbox.co>
2017-05-17 21:38:06 +0500

    migrations modified

--- db/migrate/20160714014047_create_shares.rb
+++ db/migrate/20160714014047_create_shares.rb
@@ -4,7 +4,7 @@ class CreateShares < ActiveRecord::Migration
       t.string :type
       t.integer :price
       t.belongs_to :project, index: true, foreign_key: true
-      t.belongs_to :transaction, index: true, foreign_key: true
+      # t.belongs_to :transaction, index: true, foreign_key: true
 
       t.timestamps null: false
     end
